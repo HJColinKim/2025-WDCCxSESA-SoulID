9ea015f9a5ce2c8fe3d3303b625427423c62eac5
Sebastian <sebastian.camus@goguided.nz>
2025-08-02 17:53:14 +1200

    feat: add Bouncing DVD Logo ad type and integrate into AdManager

--- app/adsManager.tsx
+++ app/adsManager.tsx
@@ -4,6 +4,7 @@ import WinRARPopup from './ads/winrar';
 import NormalAdPopup from './ads/cd';
 import ExplodeAdPopup from './ads/explode';
 import NigerianPrincePopup from './ads/crazy'; 
+import BouncingDVDLogo from './ads/dvd';
 import { AdType } from './types';
 
 
@@ -22,7 +23,7 @@ const AdManagerContext = createContext<AdManagerContextType | undefined>(undefin
 export const AdProvider: FC<{ children: ReactNode }> = ({ children }) => {
   // 1. State is now an array of ad instances, not a single ad
   const [activeAds, setActiveAds] = useState<AdInstance[]>([]);
-  const adOptions: AdType[] = ['winrar', 'normal', 'explode', 'crazy'];
+  const adOptions: AdType[] = ['winrar', 'normal', 'explode', 'crazy', 'dvd'];
 
   const showAdPopup = () => {
     const newAd: AdInstance = {
@@ -55,6 +56,9 @@ export const AdProvider: FC<{ children: ReactNode }> = ({ children }) => {
         }
         if (ad.type == 'crazy') {
           return <NigerianPrincePopup key={ad.id} onClose={() => closeAdPopup(ad.id)} />;
+        }
+        if (ad.type === 'dvd') {
+          return <BouncingDVDLogo key={ad.id} />;
         }
         return null;
       })}
